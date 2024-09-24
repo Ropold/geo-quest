267068801303c415df693940b1469a6cd6288351
Robert Stolz <robert.stolz87@gmail.com>
2024-09-24 19:07:54 +0200

    search ohne pg

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,13 @@
 class Deck < ApplicationRecord
   belongs_to :user
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_country,
+                  against: :country,
+                  using: {
+                    tsearch: { prefix: true }, # Erlaubt die Suche mit Präfix
+                    trigram: { threshold: 0.1 } # Trigramm-Ähnlichkeitssuche
+                  }
 end
+
